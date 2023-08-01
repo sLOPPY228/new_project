8895abe1843efa560ea50e34c6fbe2ba4d97084e
Ashok Ranjitkar <ashokranjitkar228@gmail.com>
2023-08-01 08:30:39 +0545

    calculator

---
+++ calculator.c
@@ -0,0 +1,11 @@
+// c program to calclulator
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    c = a + b;
+    printf("Sum of two numbers is : %d",c);
+    return 0;
+    }
+    
